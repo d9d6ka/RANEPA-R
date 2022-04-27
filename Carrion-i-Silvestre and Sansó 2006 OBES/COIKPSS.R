24f59d4bcad7db8bb1b4e478b2850e48ca123281
Vadim Zyamalov <zyamalov.ve@gmail.com>
2022-04-27 21:08:08 +0300

    Carrion-i-Silvestre and Sansó 2006 OBES
    Функция determi

--- Carrion-i-Silvestre and Sansó 2006 OBES/COIKPSS.R
+++ Carrion-i-Silvestre and Sansó 2006 OBES/COIKPSS.R
@@ -1,1 +1,46 @@
 # Carrion-i-Silvestre and Sansó 2006 OBES
+
+determi <- function(model, t, tb) {
+    du <- rbind(
+        matrix(data = 0, nrow = tb, ncol = 1),
+        matrix(data = 1, nrow = t - tb, ncol = 1)
+    )
+    dt <- rbind(
+        matrix(data = 0, nrow = tb, ncol = 1),
+        matrix(data = 1:(t - tb), nrow = t - tb, ncol = 1)
+    )
+
+    if (model == 1) {
+        xt <- cbind(
+            matrix(data = 1, nrow = t, ncol = 1),
+            du
+        )
+    }
+    else if (model == 2) {
+        xt <- cbind(
+            matrix(data = 1, nrow = t, ncol = 1),
+            du,
+            matrix(data = 1:t, nrow = t, ncol = 1)
+        )
+    }
+    else if (model == 3) {
+        xt <- cbind(
+            matrix(data = 1, nrow = t, ncol = 1),
+            matrix(data = 1:t, nrow = t, ncol = 1),
+            dt
+        )
+    }
+    else if (model == 4) {
+        xt <- cbind(
+            matrix(data = 1, nrow = t, ncol = 1),
+            du,
+            matrix(data = 1:t, nrow = t, ncol = 1),
+            dt
+        )
+    }
+    else {
+        stop("ERROR: Try to specify the deterministic component again")
+    }
+
+    return(xt)
+}
