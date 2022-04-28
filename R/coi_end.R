bf4ea89fef1cbd5423b764325b806effbe131ba3
Vadim Zyamalov <zyamalov.ve@gmail.com>
2022-04-28 20:10:16 +0300

    Carrion-i-Silvestre and Sansó 2006 OBES
    Функция coi_kpss

--- R/coi_end.R
+++ R/coi_end.R
@@ -4,6 +4,67 @@ require(zeallot)
 
 ## `[` <- function(...) base::`[`(..., drop = FALSE) # nolint
 
+coi_kpss <- function(y, x, model, tb, k2, cri) {
+    if (!is.matrix(y)) y <- as.matrix(y)
+    if (!is.matrix(x)) x <- as.matrix(x)
+
+    t <- nrow(y)
+
+    if (model < 0 & model > 6)
+        stop("ERROR: Try to specify the deterministic component again")
+
+    if (cri[1] == 0) {
+        if (model == 0)
+            xt <- x
+        else if (1 <= model & model <= 4) {
+            deter <- determi(model, t, tb)
+            xt <- cbind(deter, x)
+        }
+        else if (model == 5) {
+            deter <- determi(1, t, tb)
+            xdu <- sweep(x, 1, deter[, 2, drop = FALSE], `*`)
+            xt <- cbind(deter, x, xdu)
+        }
+        else if (model == 6) {
+            deter <- determi(4, t, tb)
+            xdu <- sweep(x, 1, deter[, 2, drop = FALSE], `*`)
+            xt <- cbind(deter, x, xdu)
+        }
+
+        beta <- solve(t(xt) %*% xt) %*% t(xt) %*% y
+        u <- y - xt %*% beta
+        t_b <- tb
+    }
+    else if (cri[1] == 1) {
+        k <- cri[2]
+        bic_op <- 100000000
+        for (i in k:1) {
+            c(beta, u, bic, t_b) %<-% dols(y, x, model, k, k, tb)
+            if (bic < bic_op) {
+                bic_op <- bic
+                beta_op <- beta
+                t_b_op <- t_b
+                u_op <- u
+            }
+        }
+        u <- u_op
+        beta <- beta_op
+        t_b <- t_b_op
+    }
+
+    sg <- alrvr(u)
+    tests <- solve((t^2) * sg) * apply(apply(u, 2, cumsum)^2, 2, sum)
+
+    return(
+        list(
+            beta = beta,
+            tests = tests,
+            u = u,
+            t_b = t_b
+        )
+    )
+}
+
 determi <- function(model, t, tb) {
     du <- rbind(
         matrix(data = 0, nrow = tb, ncol = 1),
@@ -50,7 +111,9 @@ determi <- function(model, t, tb) {
 }
 
 alrvr <- function(e) {
-    t <- length(e)
+    if (!is.matrix(e)) e <- as.matrix(e)
+
+    t <- nrow(e)
     k <- 0.8
     a <- solve(t(e[1:(t - 1)]) %*% e[1:(t - 1)]) %*% t(e[1:(t - 1)]) %*% e[2:t]
     l <- min(
@@ -61,7 +124,7 @@ alrvr <- function(e) {
     lrv <- as.numeric((t(e) %*% e) / t)
     for (i in 1:l) {
         w <- (1 - i / (l + 1))
-        lrv <- lrv + 2 * as.numeric(t(e[1:(t - 1)]) %*% e[(1 + i):t]) * w / t
+        lrv <- lrv + 2 * as.numeric(t(e[1:(t - i)]) %*% e[(1 + i):t]) * w / t
     }
     return(lrv)
 }
@@ -86,12 +149,12 @@ lagn <- function(x, i) {
         )
 }
 
-DOLS <- function(y, x, model, klags, kleads, tb) {
+dols <- function(y, x, model, klags, kleads, tb) {
     if (!is.matrix(y)) y <- as.matrix(y)
     if (!is.matrix(x)) x <- as.matrix(x)
-    
+
     t <- nrow(y)
-    
+
     streg <- x
     d_streg <- streg[2:t, , drop = FALSE] - streg[1:(t - 1), , drop = FALSE]
     d_streg_step <- d_streg
@@ -181,4 +244,29 @@ DOLS <- function(y, x, model, klags, kleads, tb) {
             t_beta = t_beta
         )
     )
+}
+
+valors <- function(model, lambda, k) {
+    load("data/coi_end_vc.data")
+    m_vc <- coi_end_vc[[model]][[k]]
+    if (0 < lambda & lambda <= 0.15)
+        v_vc <- m_vc[, 1, drop = FALSE]
+    else if (0.15 < lambda & lambda <= 0.25)
+        v_vc <- m_vc[, 2, drop = FALSE]
+    else if (0.25 < lambda & lambda <= 0.35)
+        v_vc <- m_vc[, 3, drop = FALSE]
+    else if (0.35 < lambda & lambda <= 0.45)
+        v_vc <- m_vc[, 4, drop = FALSE]
+    else if (0.45 < lambda & lambda <= 0.55)
+        v_vc <- m_vc[, 5, drop = FALSE]
+    else if (0.55 < lambda & lambda <= 0.65)
+        v_vc <- m_vc[, 6, drop = FALSE]
+    else if (0.65 < lambda & lambda <= 0.75)
+        v_vc <- m_vc[, 7, drop = FALSE]
+    else if (0.75 < lambda & lambda <= 0.85)
+        v_vc <- m_vc[, 8, drop = FALSE]
+    else if (0.85 < lambda & lambda < 1)
+        v_vc <- m_vc[, 9, drop = FALSE]
+    else
+        stop("ERROR! Try to specify the value of lambda again")
 }
