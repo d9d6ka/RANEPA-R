05b8e993db0871fbd4a3d2eaa961b0e43678770f
Vadim Zyamalov <zyamalov.ve@gmail.com>
2022-04-29 19:12:41 +0300

    Добавлен комментарий к alrvr

--- R/alrvr.R
+++ R/alrvr.R
@@ -1,3 +1,5 @@
+## Procedure ALRVR to estimate the long-run variance as
+## in Andrews (1991) and Kurozumi (2002).
 alrvr <- function(e) {
     if (!is.matrix(e)) e <- as.matrix(e)
 
@@ -9,7 +11,7 @@ alrvr <- function(e) {
         1.1447 * (4 * k^2 * t / ((1 + k)^2 * (1 - k)^2))^(1 / 3)  # nolint
     )
     l <- trunc(l)
-    lrv <- as.numeric((t(e) %*% e) / t)
+    lrv <- as.numeric(t(e) %*% e) / t
     for (i in 1:l) {
         w <- (1 - i / (l + 1))
         lrv <- lrv + 2 * as.numeric(t(e[1:(t - i)]) %*% e[(1 + i):t]) * w / t
