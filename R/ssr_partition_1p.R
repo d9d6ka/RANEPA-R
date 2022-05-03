4df7008efc79d9deb44cfaf2d8c3aaf03c00c301
Vadim Zyamalov <zyamalov.ve@gmail.com>
2022-05-03 21:58:18 +0300

    Исправление логики
    Cadlag, кто бы мог подумать. Век живи --- век учись!

--- R/ssr_partition_1p.R
+++ R/ssr_partition_1p.R
@@ -2,8 +2,8 @@ ssr_partition_1p <- function(beg, end, first_b, last_b, len, ssr_data) {
     tmp_result <- matrix(data = Inf, nrow = len, ncol = 1)
 
     for (p_break in first_b:last_b) {
-        tmp_result[p_break] <- ssr_data[beg, p_break - 1] +
-            ssr_data[p_break, end]
+        tmp_result[p_break] <- ssr_data[beg, p_break] +
+            ssr_data[p_break + 1, end]
     }
 
     tmp_ssr <- min(tmp_result[first_b:last_b])
--- R/ssr_partition_mp.R
+++ R/ssr_partition_mp.R
@@ -2,21 +2,23 @@
 #'
 #' @details
 #' Based on Bai and Perron (2003).
-#' 
+#'
 #' @param y (Tx1)-vector of the dependent variable
 #' @param x (Txk)-vector of the explanatory stochastic regressors
 #' @param m Number of breaks
 #' @param width Minimum spacing between the breaks
-#' 
+#'
 #' @return List of 2 elements: optimal SSR and the vector of breaks
-#' 
+#'
 #' @export
-ssr_partition_mp <- function(y, x, m = 1, width = 2) {
+ssr_partition_mp <- function(y, x, m = 1, width = 2, ssr_data = NULL) {
     if (!is.matrix(y)) y <- as.matrix(y)
     if (!is.matrix(x)) x <- as.matrix(x)
 
     t <- nrow(y)
-    ssr_data <- ssr_matrix(y, x, width)
+    
+    if (is.null(ssr_data))
+        ssr_data <- ssr_matrix(y, x, width)
 
     if (m == 1) {
         tmp_result <- ssr_partition_1p(
@@ -34,22 +36,17 @@ ssr_partition_mp <- function(y, x, m = 1, width = 2) {
             nrow = variants,
             ncol = 1
         )
-        loop_ends <- matrix(
-            data = 0,
-            nrow = variants,
-            ncol = 1
-        )
         loop_break <- matrix(
             data = 0,
             nrow = variants,
             ncol = m
         )
-        tmp_ssr <- matrix(
-            data = Inf,
-            nrow = variants,
-            ncol = 1
-        )
         for (step in 1:m) {
+            tmp_ssr <- matrix(
+                data = Inf,
+                nrow = variants,
+                ncol = 1
+            )
             if (step == 1) {
                 for (v in 1:variants) {
                     loop_end <- 2 * width + v - 1
@@ -63,18 +60,17 @@ ssr_partition_mp <- function(y, x, m = 1, width = 2) {
                     )
                     loop_ssr[v, 1] <- tmp_res$ssr
                     loop_break[v, 1] <- tmp_res$break_point
-                    loop_ends[v, 1] <- loop_end
                 }
             }
             else if (step == m) {
                 for (v in 1:variants) {
                     tmp_ssr[v, 1] <- loop_ssr[v, 1] +
-                        ssr_data[step * width + v, t]
+                        ssr_data[step * width + v - 1, t]
                 }
                 optimal_ssr <- min(tmp_ssr)
                 optimal_index <- which.min(tmp_ssr)
                 optimal_break <- loop_break[optimal_index, ]
-                optimal_break[m] <- step * width + optimal_index
+                optimal_break[m] <- step * width + optimal_index - 1
             }
             else {
                 next_ssr <- matrix(
@@ -91,13 +87,12 @@ ssr_partition_mp <- function(y, x, m = 1, width = 2) {
                     next_v <- loop_end - (step + 1) * width + 1
                     for (v in 1:variants) {
                         tmp_ssr[v, 1] <- loop_ssr[v, 1] +
-                            ssr_data[step * width + v, loop_end]
+                            ssr_data[step * width + v - 1, loop_end]
                     }
                     next_ssr[next_v, 1] <- min(tmp_ssr)
                     next_index <- which.min(tmp_ssr)
                     next_break[next_v, 1:m] <- loop_break[next_index, ]
-                    next_break[next_v, step] <- step * width + next_index
-                    loop_ends[next_v, 1] <- loop_end
+                    next_break[next_v, step] <- step * width + next_index - 1
                 }
                 loop_ssr <- next_ssr
                 loop_break <- next_break
