89db10c8c4c882de4394f15d870f321243762951
Vadim Zyamalov <zyamalov.ve@gmail.com>
2022-05-22 09:31:03 +0300

    Тесты на пузыри в данных
    Добавил последние функции для тестов на наличие пузырей в данных:
    бутстрапы и взвешенные тесты.

--- R/weighted.GSADF.test.R
+++ R/weighted.GSADF.test.R
@@ -1,108 +1,140 @@
-# weighted.GSADF.test - Weighted SADF test (HLZ, 2018).
-weighted.GSADF.test <- function(y, r0 = 0.01 + 1.8/sqrt(length(y)), const = TRUE,
-                                B = 200, alpha = 0.05, cpu = 4, urs = TRUE,
-                                seed = round(10^4*sd(y))) {
-  result <- list()
-  result$y <- y
-  N <- length(y)
-  result$r0 <- r0
-  result$const <- const
-  result$B <- B
-  result$alpha <- alpha
-  result$cpu <- cpu
-  result$urs <- urs
-  result$seed <- seed
-
-  # Find supBZ_value.
-  supBZ.model <- supBZ.statistic(y, r0)
-  sigma_sq <- supBZ.model$sigma_sq
-  result$sigma_sq <- sigma_sq
-  result$BZ_values <- supBZ.model$BZ_values
-  result$supBZ_value <- supBZ.model$supBZ_value
-
-  # Auxiliary function for parallel.
-  calc_sadf_supBZ_value <- function(cpu, y, r0, const, B, seed, urs, sigma_sq) {
-    set.seed(seed + cpu)
+#' @title
+#' Weighted GSADF test (HLZ, 2018).
+#'
+#' @import doSNOW
+#' @import foreach
+#' @import parallel
+#'
+#' @export
+weighted.GSADF.test <- function(y,
+                                r0 = 0.01 + 1.8 / sqrt(length(y)),
+                                const = TRUE,
+                                alpha = 0.05,
+                                iter = 4 * 200,
+                                urs = TRUE,
+                                seed = round(10^4 * sd(y))) {
     N <- length(y)
-    result <- list()
-    for(i in 1:B) {
-      y_star <- cumsum(c(0, rnorm(N-1)*diff(y)))
-      if (urs == TRUE) {
-        gsadf.model <- GSADF.test(y_star, r0, const)
-        result$sadf_value[i] <- gsadf.model$sadf_value
-      }
-      supBZ.model <- supBZ.statistic(y_star, r0, sigma_sq)
-      result$supBZ_value[i] <- supBZ.model$supBZ_value
+
+    # Find supBZ.value.
+    supBZ.model <- supBZ.statistic(y, r0)
+    sigma.sq <- supBZ.model$sigma.sq
+    BZ.values <- supBZ.model$BZ.values
+    supBZ.value <- supBZ.model$supBZ.value
+
+    # Do parallel.
+    cores <- detectCores()
+
+    progress.bar <- txtProgressBar(max = iter, style = 3)
+    progress <- function(n) setTxtProgressBar(progress.bar, n)
+
+    cluster <- makeCluster(max(cores - 1, 1))
+    clusterExport(cluster, c("ADF.test",
+                             "GSADF.test",
+                             "supBZ.statistic",
+                             ".cval_GSADF_without_const",
+                             ".cval_GSADF_with_const"))
+    registerDoSNOW(cluster)
+
+    SADF.supBZ.bootstrap.values <- foreach(
+        step = 1:iter,
+        .combine = "rbind",
+        .options.snow = list(progress = progress)
+    ) %dopar% {
+        y.star <- cumsum(c(0, rnorm(N - 1) * diff(y)))
+        tmp.gsadf.value <- NA
+        if (urs == TRUE) {
+            gsadf.model <- GSADF.test(y.star, r0, const)
+            tmp.gsadf.value <- gsadf.model$gsadf.value
+        }
+        supBZ.model <- supBZ.statistic(y.star, r0, sigma.sq)
+        tmp.supBZ.value <- supBZ.model$supBZ.value
+        c(tmp.gsadf.value, tmp.subBZ.value)
     }
-    return(result)
-  }
 
-  # Do parallel.
-  cl <- makePSOCKcluster(rep("localhost", cpu))
-  clusterExport(cl, c("GSADF.test", "ADF.test", "supBZ.statistic",
-                      "GSADF_cr_values_with_const",
-                      "GSADF_cr_values_without_const"))
-  sadf_supBZ_bootstrap_values <- parLapply(cl, 1:cpu, calc_sadf_supBZ_value, y,
-                                           r0, const, B, seed, urs, sigma_sq)
-  stopCluster(cl)
+    stopCluster(cluster)
 
-  # Get sadf_supBZ_bootstrap_values.
-  result$supBZ_bootstrap_values <- c()
-  for(i in 1:cpu) {
-      result$supBZ_bootstrap_values <- c(result$supBZ_bootstrap_values,
-        sadf_supBZ_bootstrap_values[[i]]$supBZ_value)
-  }
-
-  # Find critical value.
-  result$supBZ_cr_value <- as.numeric(quantile(result$supBZ_bootstrap_values,
-                                               1-alpha))
-
-  # A union of rejections strategy.
-  if (urs == TRUE) {
-
-    # Find sadf_value.
-    gsadf.model <- GSADF.test(y, r0, const)
-    result$t.values <- gsadf.model$t.values
-    result$sadf_value <- gsadf.model$sadf_value
-
-    # Get sadf_supBZ_bootstrap_values.
-    result$sadf_bootstrap_values <- c()
-    for(i in 1:cpu) {
-      result$sadf_bootstrap_values <- c(result$sadf_bootstrap_values,
-                                        sadf_supBZ_bootstrap_values[[i]]$sadf_value)
-    }
+    # Get sadf_supBZ.bootstsrap.values.
+    supBZ.bootstsrap.values <- SADF.supBZ.bootstrap.values[, 2]
 
     # Find critical value.
-    result$sadf_cr_value <- as.numeric(quantile(result$sadf_bootstrap_values,
-                                                1-alpha))
+    supBZ.cr.value <- as.numeric(quantile(
+        supBZ.bootstsrap.values,
+        1 - alpha
+    ))
 
-    # Calculate U value.
-    result$U_value <- max(result$sadf_value,
-                          result$sadf_cr_value / result$supBZ_cr_value *
-                            result$supBZ_value)
+    t.values <- NULL
+    gsadf.value <- NULL
+    GSADF.bootstsrap.values <- NULL
+    GSADF.cr.value <- NULL
+    U.value <- NULL
+    U.bootstsrap.values <- NULL
+    U.cr.value <- NULL
 
-    # Find U_bootstrap_values.
-    result$U_bootstrap_values <- c()
-    for(b in 1:(cpu*B)) {
-      result$U_bootstrap_values[b] <- max(result$sadf_bootstrap_values[b],
-                                         result$sadf_cr_value /
-                                           result$supBZ_cr_value *
-                                           result$supBZ_bootstrap_values[b])
+    # A union of rejections strategy.
+    if (urs == TRUE) {
+        # Find sadf.value.
+        gsadf.model <- GSADF.test(y, r0, const)
+        t.values <- gsadf.model$t.values
+        gsadf.value <- gsadf.model$gsadf.value
+
+        # Get sadf_supBZ.bootstsrap.values.
+        GSADF.bootstsrap.values <- SADF.supBZ.bootstrap.values[, 1]
+
+        # Find critical value.
+        GSADF.cr.value <- as.numeric(quantile(
+            GSADF.bootstsrap.values,
+            1 - alpha
+        ))
+
+        # Calculate U value.
+        U.value <- max(
+            gsadf.value,
+            GSADF.cr.value / supBZ.cr.value * supBZ.value
+        )
+
+        # Find U.bootstsrap.values.
+        U.bootstsrap.values <- c()
+        for (b in 1:iter) {
+            U.bootstsrap.values[b] <- max(
+                GSADF.bootstsrap.values[b],
+                GSADF.cr.value / supBZ.cr.value * supBZ.bootstsrap.values[b]
+            )
+        }
+
+        # Find critical value.
+        U.cr.value <- as.numeric(quantile(U.bootstsrap.values, 1 - alpha))
+
+        p.value <- round(sum(U.bootstsrap.values > U.value) / iter, 4)
+
+        is.explosive <- ifelse(U.value > U.cr.value, 1, 0)
+    } else {
+        p.value <- round(sum(supBZ.bootstsrap.values > supBZ.value) / iter, 4)
+
+        is.explosive <- ifelse(supBZ.value > supBZ.cr.value, 1, 0)
     }
 
-    # Find critical value.
-    result$U_cr_value <- as.numeric(quantile(result$U_bootstrap_values, 1-alpha))
-
-    result$p_value <- round(sum(result$U_bootstrap_values >
-                                  result$U_value)/(cpu*B), 4)
-
-    result$is_explosive <- ifelse(result$U_value > result$U_cr_value, 1, 0)
-  } else {
-    result$p_value <- round(sum(result$supBZ_bootstrap_values >
-                                  result$supBZ_value)/(cpu*B), 4)
-
-    result$is_explosive <- ifelse(result$supBZ_value > result$supBZ_cr_value, 1, 0)
-  }
-
-  return(result)
+    return(
+        list(
+            y = y,
+            r0 = r0,
+            const = const,
+            alpha = alpha,
+            iter = iter,
+            urs = urs,
+            seed = seed,
+            sigma.sq = sigma.sq,
+            BZ.values = BZ.values,
+            supBZ.value = supBZ.value,
+            supBZ.bootstsrap.values = supBZ.bootstsrap.values,
+            supBZ.cr.value = supBZ.cr.value,
+            t.values = t.values,
+            gsadf.value = gsadf.value,
+            GSADF.cr.value = GSADF.cr.value,
+            U.value = U.value,
+            U.bootstsrap.values = U.bootstsrap.values,
+            U.cr.value = U.cr.value,
+            p.value = p.value,
+            is.explosive = is.explosive
+        )
+    )
 }
--- R/weighted.SADF.test.R
+++ R/weighted.SADF.test.R
@@ -21,23 +21,6 @@ weighted.SADF.test <- function(y,
     BZ.values <- supBZ.model$BZ.values
     supBZ.value <- supBZ.model$supBZ.value
 
-    # Auxiliary function for parallel.
-    calc_sadf_supBZ.value <- function(cpu, y, r0, const, B, seed, urs, sigma_sq) {
-        set.seed(seed + cpu)
-        N <- length(y)
-        result <- list()
-        for (i in 1:B) {
-            y_star <- cumsum(c(0, rnorm(N - 1) * diff(y)))
-            if (urs == TRUE) {
-                sadf.model <- SADF.test(y_star, r0, const)
-                result$sadf_value[i] <- sadf.model$sadf_value
-            }
-            supBZ.model <- supBZ.statistic(y_star, r0, sigma_sq)
-            result$supBZ.value[i] <- supBZ.model$supBZ.value
-        }
-        return(result)
-    }
-
     # Do parallel.
     cores <- detectCores()
 
@@ -52,88 +35,81 @@ weighted.SADF.test <- function(y,
                              ".cval_SADF_with_const"))
     registerDoSNOW(cluster)
 
-
-
-    cl <- makePSOCKcluster(rep("localhost", cpu))
-    clusterExport(cl, c(
-        "SADF.test", "ADF.test", "supBZ.statistic",
-        "SADF_cr_values_with_const",
-        "SADF_cr_values_without_const"
-    ))
-    sadf_supBZ_bootstrap_values <- parLapply(
-        cl, 1:cpu, calc_sadf_supBZ.value, y,
-        r0, const, B, seed, urs, sigma.sq
-    )
-    stopCluster(cl)
-
-    # Get sadf_supBZ_bootstrap_values.
-    result$supBZ_bootstrap_values <- c()
-    for (i in 1:cpu) {
-        result$supBZ_bootstrap_values <- c(
-            result$supBZ_bootstrap_values,
-            sadf_supBZ_bootstrap_values[[i]]$supBZ.value
-        )
+    SADF.supBZ.bootstrap.values <- foreach(
+        step = 1:iter,
+        .combine = "rbind",
+        .options.snow = list(progress = progress)
+    ) %dopar% {
+        y.star <- cumsum(c(0, rnorm(N - 1) * diff(y)))
+        tmp.sadf.value <- NA
+        if (urs == TRUE) {
+            sadf.model <- SADF.test(y.star, r0, const)
+            tmp.sadf.value <- sadf.model$sadf.value
+        }
+        supBZ.model <- supBZ.statistic(y.star, r0, sigma.sq)
+        tmp.supBZ.value <- supBZ.model$supBZ.value
+        c(tmp.sadf.value, tmp.subBZ.value)
     }
 
+    stopCluster(cluster)
+
+    # Get sadf_supBZ_bootstrap.values.
+    supBZ.bootstrap.values <- SADF.supBZ.bootstrap.values[, 2]
+
     # Find critical value.
-    result$supBZ_cr_value <- as.numeric(quantile(
-        result$supBZ_bootstrap_values,
+    supBZ.cr.value <- as.numeric(quantile(
+        supBZ.bootstrap.values,
         1 - alpha
     ))
 
+    t.values <- NULL
+    sadf.value <- NULL
+    SADF.bootstrap.values <- NULL
+    SADF.cr.value <- NULL
+    U.value <- NULL
+    U.bootstrap.values <- NULL
+    U.cr.value <- NULL
+
     # A union of rejections strategy.
     if (urs == TRUE) {
-
         # Find sadf_value.
         sadf.model <- SADF.test(y, r0, const)
-        result$t.values <- sadf.model$t.values
-        result$sadf_value <- sadf.model$sadf_value
+        t.values <- sadf.model$t.values
+        sadf.value <- sadf.model$sadf.value
 
-        # Get sadf_supBZ_bootstrap_values.
-        result$sadf_bootstrap_values <- c()
-        for (i in 1:cpu) {
-            result$sadf_bootstrap_values <- c(
-                result$sadf_bootstrap_values,
-                sadf_supBZ_bootstrap_values[[i]]$sadf_value
-            )
-        }
+        # Get sadf_supBZ.bootstrap.values.
+        SADF.bootstrap.values <- SADF.supBZ.bootstrap.values[, 1]
 
         # Find critical value.
-        result$sadf_cr_value <- as.numeric(quantile(
-            result$sadf_bootstrap_values,
-            1 - alpha
-        ))
+        SADF.cr.value <- as.numeric(quantile(SADF.bootstrap.values, 1 - alpha))
 
         # Calculate U value.
-        result$U_value <- max(
-            result$sadf_value,
-            result$sadf_cr_value / result$supBZ_cr_value *
-                result$supBZ.value
+        U.value <- max(
+            sadf.value,
+            SADF.cr.value / supBZ.cr.value * supBZ.value
         )
 
-        # Find U_bootstrap_values.
-        result$U_bootstrap_values <- c()
-        for (b in 1:(cpu * B)) {
-            result$U_bootstrap_values[b] <- max(
-                result$sadf_bootstrap_values[b],
-                result$sadf_cr_value /
-                    result$supBZ_cr_value *
-                    result$supBZ_bootstrap_values[b]
+        # Find U_bootstrap.values.
+        U.bootstrap.values <- c()
+        for (b in 1:iter) {
+            U.bootstrap.values[b] <- max(
+                SADF.bootstrap.values[b],
+                SADF.cr.value /
+                    supBZ.cr.value *
+                    supBZ.bootstrap.values[b]
             )
         }
 
         # Find critical value.
-        result$U_cr_value <- as.numeric(quantile(result$U_bootstrap_values, 1 - alpha))
+        U.cr.value <- as.numeric(quantile(U.bootstrap.values, 1 - alpha))
 
-        result$p_value <- round(sum(result$U_bootstrap_values >
-            result$U_value) / (cpu * B), 4)
+        p.value <- round(sum(U.bootstrap.values > U.value) / iter, 4)
 
-        result$is_explosive <- ifelse(result$U_value > result$U_cr_value, 1, 0)
+        is.explosive <- ifelse(U.value > U.cr.value, 1, 0)
     } else {
-        result$p_value <- round(sum(result$supBZ_bootstrap_values >
-            result$supBZ.value) / (cpu * B), 4)
+        p.value <- round(sum(supBZ.bootstrap.values > supBZ.value) / iter, 4)
 
-        result$is_explosive <- ifelse(result$supBZ.value > result$supBZ_cr_value, 1, 0)
+        is.explosive <- ifelse(supBZ.value > supBZ.cr.value, 1, 0)
     }
 
     return(
@@ -148,7 +124,16 @@ weighted.SADF.test <- function(y,
             sigma.sq = sigma.sq,
             BZ.values = BZ.values,
             supBZ.value = supBZ.value,
-
+            supBZ.bootstrap.values = supBZ.bootstrap.values,
+            supBZ.cr.value = supBZ.cr.value,
+            t.values = t.values,
+            sadf.value = sadf.value,
+            SADF.cr.value = SADF.cr.value,
+            U.value = U.value,
+            U.bootstrap.values = U.bootstrap.values,
+            U.cr.value = U.cr.value,
+            p.value = p.value,
+            is.explosive = is.explosive
         )
     )
 }
