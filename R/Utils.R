1024654ba08bcb069e98485b893c30d9860603a9
Vadim Zyamalov <zyamalov.ve@gmail.com>
2022-09-20 13:26:47 +0300

    Убираем ненужную функцию

--- R/Utils.R
+++ R/Utils.R
@@ -33,38 +33,6 @@ lagn <- function(x,
 
 
 #' @title
-#' Calculating ACF values
-#'
-#' @description
-#' A simple auxiliary function providing the estimates of autocorrelations of
-#' orders from \eqn{0} to \eqn{N - 1}.
-#'
-#' @param y An input time series of interest.
-#'
-#' @return The vector of ACF values of orders from 0 to \eqn{N - 1}.
-#' Due to the R's way of indexing the 1-st element is the autocorrelation of
-#' order 0, and the N-th value is the autcorrelation of order \eqn{N - 1}.
-#'
-#' @keywords internal
-ACF <- function(y) {
-    if (!is.matrix(y)) y <- as.matrix(y)
-
-    n.obs <- nrow(y)
-
-    mean.y <- mean(y)
-
-    result <- rep(0, n.obs)
-
-    for (i in 0:(n.obs - 1)) {
-        result[i + 1] <-
-            t(y[1:(n.obs - i)] - mean.y) %*% (y[(1 + i):n.obs] - mean.y) / n.obs
-    }
-
-    return(result)
-}
-
-
-#' @title
 #' Auxiliary function returning KPSS statistic value.
 #'
 #' @param resids The series of residuals.
