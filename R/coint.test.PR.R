ddcb804456c549ab1112e4eda7bde8918febeffb
Vadim Zyamalov <zyamalov.ve@gmail.com>
2022-09-22 23:37:35 +0300

    Продолжаем работать над новым кодом

--- R/coint.test.PR.R
+++ R/coint.test.PR.R
@@ -1,4 +1,4 @@
-coint.test.PR <- function(y, x, det_comp, kmin = 0) {
+coint.test.PR <- function(y, x, det.comp, kmin = 0) {
     n.obs <- nrow(y)
     n.var <- ncol(x)
 
@@ -14,114 +14,114 @@ coint.test.PR <- function(y, x, det_comp, kmin = 0) {
         options <- c(-30.00, -33.75, -29.75)
     }
 
-    opt_cbar <- options[det_comp]
+    opt.cbar <- options[det.comp]
 
-    kmax <- round(4 * (n.obs / 100)^0.25)
+    kmax <- round(4 * (n.obs / 100)^(1 / 4))
 
-    z <- if (det_comp == 1 || det_comp == 3) {
+    z <- if (det.comp == 1 || det.comp == 3) {
         as.matrix(rep(1, n.obs))
-    } else if (det_comp == 2) {
+    } else if (det.comp == 2) {
         cbind(rep(1, n.obs), (1:n.obs))
     }
 
-    y_d <- GLS(y, z, opt_cbar)$residuals
-    x_d <- GLS(x, z, opt_cbar)$residuals
+    y_d <- GLS(y, z, opt.cbar)$residuals
+    x_d <- GLS(x, z, opt.cbar)$residuals
 
-    model <- lm.fit(x_d, y_d)
-    ud_hat <- cbind(model$residuals)
+    model <- OLS(y_d, x_d)
+    ud.hat <- cbind(model$residuals)
 
-    result <- resid.tests.PR(ud_hat, kmin, kmax, opt_cbar, det_comp)
+    result <- resid.tests.PR(ud.hat, kmin, kmax, opt.cbar, det.comp)
 
     return(result)
 }
 
 
-resid.tests.PR <- function(ud_hat, kmin, kmax, opt_cbar, det_comp) {
+resid.tests.PR <- function(ud.hat, kmin, kmax, opt.cbar, det.comp) {
     result <- list()
 
-    n.obs <- nrow(ud_hat)
+    n.obs <- nrow(ud.hat)
 
-    if (ncol(ud_hat) > 1) {
+    if (ncol(ud.hat) > 1) {
         stop("ERROR:")
     }
 
-    gls_tests <- matrix(0, 7, 1)
-    minbicgls <- Inf
+    gls.tests <- matrix(0, 7, 1)
+    min.bic <- Inf
 
-    lm_model <- lm.fit(
-        as.matrix(ud_hat[(1:(nrow(ud_hat) - 1)), 1]),
-        as.matrix(ud_hat[(2:nrow(ud_hat)), 1])
+    ud.hat.1 <- cbind(ud.hat[(1:(nrow(ud.hat) - 1)), 1])
+
+    model.1 <- OLS(
+        as.matrix(ud.hat[(2:nrow(ud.hat)), 1]),
+        ud.hat.1
     )
-    alpha_1 <- cbind(lm_model$coefficients)
-    ee_1 <- cbind(lm_model$residuals)
 
-    ud_hat_1 <- cbind(ud_hat[(1:(nrow(ud_hat) - 1)), 1])
+    alpha.1 <- cbind(model.1$beta)
+    ee.1 <- cbind(model.1$residuals)
 
-    aa <- solve(t(ud_hat_1) %*% ud_hat_1)
-    ss <- c(t(ee_1) %*% ee_1) / (nrow(ee_1) - ncol(ud_hat_1))
-    talpha <- (alpha_1 - 1) / sqrt(ss * aa[1, 1])
-    sum_ud_hat <- sum(ud_hat[(1:(n.obs - 1)), 1]^2) / (n.obs - 1)^2
+    aa <- solve(t(ud.hat.1) %*% ud.hat.1)
+    ss <- c(t(ee.1) %*% ee.1) / (nrow(ee.1) - ncol(ud.hat.1))
+    t.alpha <- (alpha.1 - 1) / sqrt(ss * aa[1, 1])
+    sum.ud.hat <- sum(ud.hat[(1:(n.obs - 1)), 1]^2) / (n.obs - 1)^2
 
-    lag_bic <- kmin
-    while (lag_bic <= kmax) {
-        dud_hat <- diffn(ud_hat, na = 0)
-        reg <- lagn(ud_hat, 1, na = 0)
+    lag.bic <- kmin
+    while (lag.bic <= kmax) {
+        dud.hat <- diffn(ud.hat, na = 0)
+        reg <- lagn(ud.hat, 1, na = 0)
 
         h <- 1
-        while (h <= lag_bic) {
-            reg <- cbind(reg, lagn(dud_hat, h, na = 0))
+        while (h <= lag.bic) {
+            reg <- cbind(reg, lagn(dud.hat, h, na = 0))
             h <- h + 1
         }
 
-        dud_hat_1 <- as.matrix(dud_hat[(lag_bic + 2):nrow(dud_hat), ])
-        reg_1 <- as.matrix(reg[(lag_bic + 2):nrow(reg), ])
+        dud.hat.1 <- as.matrix(dud.hat[(lag.bic + 2):nrow(dud.hat), ])
+        reg.1 <- as.matrix(reg[(lag.bic + 2):nrow(reg), ])
 
-        model_2 <- lm.fit(reg_1, dud_hat_1)
+        model.2 <- OLS(dud.hat.1, reg.1)
 
-        e2 <- cbind(model_2$residuals)
-        nef2 <- nrow(e2)
-        s2e2 <- c(t(e2) %*% e2) / (nef2 - ncol(reg_1))
-        xx2 <- solve(t(reg_1) %*% reg_1)
+        ee.2 <- cbind(model.2$residuals)
+        ss.2 <- c(t(ee.2) %*% ee.2) / (nrow(ee.2) - ncol(reg.1))
+        xx.2 <- solve(t(reg.1) %*% reg.1)
 
-        if (lag_bic == 0) {
+        if (lag.bic == 0) {
             sumb <- 0
         } else {
-            sumb <- sum(model_2$coefficients[2:(lag_bic + 1)])
+            sumb <- sum(model.2$beta[2:(lag.bic + 1)])
         }
 
-        s22 <- s2e2 / ((1 - sumb)^2)
+        ss.adj <- ss.2 / ((1 - sumb)^2)
 
-        bicgls <- log(c(t(e2) %*% e2) / (n.obs - kmax)) +
-            (log(n.obs - kmax) * (lag_bic)) / (n.obs - kmax)
-        if (bicgls < minbicgls) {
-            minbicgls <- bicgls
-            kbic <- lag_bic
+        cur.bic <- log(c(t(ee.2) %*% ee.2) / (n.obs - kmax)) +
+            (log(n.obs - kmax) * (lag.bic)) / (n.obs - kmax)
+        if (cur.bic < min.bic) {
+            min.bic <- cur.bic
+            kbic <- lag.bic
 
-            gls_tests[1, 1] <- ((ud_hat[n.obs, 1]^2 / n.obs) - s22) *
-                ((2 * sum_ud_hat))^(-1)
-            gls_tests[2, 1] <- sqrt(sum_ud_hat / s22)
-            gls_tests[3, 1] <- gls_tests[1, 1] * gls_tests[2, 1]
-            gls_tests[4, 1] <- model_2$coefficients[1] / sqrt(s2e2 * xx2[1, 1])
-            gls_tests[5, 1] <- (n.obs - 1) * (c(alpha_1) - 1) -
-                0.5 * (s22 - ss) * sum_ud_hat^(-1)
-            gls_tests[6, 1] <- sqrt(ss / s22) * talpha -
-                (s22 - ss) / sqrt(4 * s22 * sum_ud_hat)
+            gls.tests[1, 1] <- ((ud.hat[n.obs, 1]^2 / n.obs) - ss.adj) *
+                ((2 * sum.ud.hat))^(-1)
+            gls.tests[2, 1] <- sqrt(sum.ud.hat / ss.adj)
+            gls.tests[3, 1] <- gls.tests[1, 1] * gls.tests[2, 1]
+            gls.tests[4, 1] <- model.2$coefficients[1] / sqrt(ss.2 * xx.2[1, 1])
+            gls.tests[5, 1] <- (n.obs - 1) * (c(alpha.1) - 1) -
+                0.5 * (ss.adj - ss) * sum.ud.hat^(-1)
+            gls.tests[6, 1] <- sqrt(ss / ss.adj) * t.alpha -
+                (ss.adj - ss) / sqrt(4 * ss.adj * sum.ud.hat)
         }
 
-        lag_bic <- lag_bic + 1
+        lag.bic <- lag.bic + 1
     }
 
-    if (det_comp == 1 || det_comp == 3) {
-        gls_tests[7, 1] <- ((opt_cbar^2) * (sum_ud_hat) -
-            (opt_cbar * (ud_hat[n.obs, 1]^2 / n.obs))) / s22
-    } else if (det_comp == 2) {
-        gls_tests[7, 1] <- ((opt_cbar^2) * (sum_ud_hat) +
-            (1 - opt_cbar) * (ud_hat[n.obs, 1]^2 / n.obs)) / s22
+    if (det.comp == 1 || det.comp == 3) {
+        gls.tests[7, 1] <- ((opt.cbar^2) * (sum.ud.hat) -
+            (opt.cbar * (ud.hat[n.obs, 1]^2 / n.obs))) / s22
+    } else if (det.comp == 2) {
+        gls.tests[7, 1] <- ((opt.cbar^2) * (sum.ud.hat) +
+            (1 - opt.cbar) * (ud.hat[n.obs, 1]^2 / n.obs)) / s22
     } else {
         stop("ERROR:")
     }
 
-    result$gls_tests <- gls_tests
+    result$gls.tests <- gls.tests
     result$kbic <- kbic
 
     return(result)
